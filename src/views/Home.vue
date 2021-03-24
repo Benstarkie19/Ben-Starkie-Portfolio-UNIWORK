379726e64dd414f7e85d93450518aca70c1aa417
Ben <benstarkie19@gmail.com>
2021-03-24 19:35:16 +0000

    Added Vue/ Fixed Nav  bar

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,17 @@
 <script>
 import { defineComponent } from 'vue'
 import ButtonRepo from '@/components/ButtonRepo.vue'
+import NavBar from '@/components/NavBar.vue'
 
 export default defineComponent({
-  components: { ButtonRepo },
+  components: { NavBar },
 })
 </script>
 
 <template>
+    <NavBar />
   <main>
+
     <div class="bg-gray-50">
       <div
         class="max-w-screen-xl px-4 py-12 mx-auto sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between"
@@ -18,10 +21,11 @@ export default defineComponent({
         >
           Ready to dive in?
           <br />
-          <span class="text-indigo-600">Vite 2.x + Vue 3.x + Tailwind 2.x</span>
+          <span class="text-indigo-600">Vite 2.x + Vue 3.x + Tailwind 2.x</span> 
         </h2>
         <div class="flex mt-8 lg:flex-shrink-0 lg:mt-0">
           <div class="inline-flex rounded-md shadow">
+            <button class="btn btn-primary rounded-full">DaisyUI Button</button>
             <router-link
               to="/about"
               class="inline-flex items-center justify-center px-5 py-3 text-base font-medium leading-6 text-white transition duration-150 ease-in-out bg-indigo-600 border border-transparent rounded-md hover:bg-indigo-500 focus:outline-none"
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,12 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  options: {
+        safelist: [
+          /data-theme$/,]},
   theme: {
     extend: {
+      colors: require('daisyui/colors'),
       fontFamily: {
         sans: ['"Inter var"', ...defaultTheme.fontFamily.sans],
       },
@@ -14,5 +18,12 @@ module.exports = {
     require('@tailwindcss/typography'),
     require('@tailwindcss/line-clamp'),
     require('@tailwindcss/aspect-ratio'),
+    require('daisyui'),
   ],
+  // config (optional)
+    daisyui: {
+      styled: true,
+      themes: true,
+      rtl: false,
+    },
 }
