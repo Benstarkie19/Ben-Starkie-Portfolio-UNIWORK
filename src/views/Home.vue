62829c08e2cf09d3e2c4935b1e5e1214826d14c1
Ben <benstarkie19@gmail.com>
2021-05-04 14:03:28 +0100

    Back to work, Started on the home page fixing bugs

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -44,30 +44,24 @@ export default defineComponent({
                     <div
                         class="flex flex-col items-center w-full pt-0 mb-16 text-left lg:flex-grow md:w-1/2 xl:mr-20 md:pr-24 md:items-start md:text-left md:mb-0 lg:text-center">
                         <h3 class="mb-5 text-2xl text-center font-bold "><span class="font-medium tracking-wide btn btn-ghost text-primary transition-colors duration-200 hover:text-teal-accent-400">0.1</span> About Me. <span class="font-medium tracking-wide text-primary transition-colors duration-200 hover:text-teal-accent-400">⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯</span></h3> 
-                    <p class="mb-8 text-base leading-relaxed text-center text-gray-1000 lg:text-left lg:text-1xl">
-                        Deploy
-                        your mvp in
-                        minutes, not days. WT offers you a a wide selection swapable sections for your landing page.
+                    <p class="mb-8 text-base leading-relaxed text-left text-gray-1000 lg:text-left lg:text-1xl">
+                      Hello! My name is Benjamin and I enjoy creating things that stay on
+                      the internet. My interest in web development started back in 2017 at
+                      college while studying computing with a twist of media studys.
+                    </p>
+                    <p class="mb-8 text-base leading-relaxed text-left text-gray-1000 lg:text-left lg:text-1xl">
+                      Coming to the end of my undergraduate degree at Edge Hill University, 
+                      studying was changeling but interesting experience within the coronavirus pandemic. 
+                      In light of recent events for my final year project, 
+                      I started developing a Student Accommodation App based on React Native for students struggling to find a place to stay while studying. 
+                      With hope to one day finish it and fully release it.
                     </p>
                     <div class="flex justify-center">
                         <button
-                            class="flex items-center px-6 py-2 mt-auto font-semibold text-white transition duration-500 ease-in-out transform bg-black rounded-lg hover:bg-gray-800 hover:to-black focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2">
-                            Action
+                              class="flex items-center px-6 py-2 mt-auto font-semibold text-white transition duration-500 ease-in-out transform bg-black rounded-lg hover:bg-gray-800 hover:to-black focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2">
+                            <a href="https://github.com/Benstarkie19">Github</a>
+                            
                         </button>
-                        <p class="mt-2 text-sm text-center text-gray-600 md:ml-6 md:mt-0 sm:text-left">
-                            It will take you to candy shop.
-                            <br class="hidden lg:block">
-                            <a href="#"
-                                class="inline-flex items-center font-semibold text-blue-700 md:mb-2 lg:mb-0 hover:text-blue-400 ">
-                                Learn More
-                                <svg class="w-4 h-4 ml-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
-                                    width="20" height="20" fill="currentColor">
-                                    <path fill="none" d="M0 0h24v24H0z" />
-                                    <path
-                                        d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z" />
-                                </svg>
-                            </a>
-                        </p>
                     </div>
                 </div>
                 <div class="w-5/6 lg:max-w-lg lg:w-full md:w-1/2">
